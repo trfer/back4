3ad31b6ed89d2d8f7e41b098b0dc1de36ba490a6
trfer <115195768+trfer@users.noreply.github.com>
2023-07-14 21:58:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@
 DISPLAY_NAME=${DISPLAY_NAME:-'Argo_xray_'}
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=${UUID:-'600bf80e-453c-4b37-97a9-ec7172581d48'}
+UUID=${UUID:-'f81aa586-7b35-4be7-b344-28bbdfda5437'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
 VMESS_WARP_WSPATH=${VMESS_WARP_WSPATH:-'/vmess_warp'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
